d2a41fee8440ca6ce98fa6959971b526edbd85cc
Kwlad1ck <vlad-kad00@mail.ru>
2024-02-05 23:23:46 +0300

    init commit

---
+++ src/test/java/tests/SelenideTest.java
@@ -0,0 +1,24 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.By.linkText;
+
+public class SelenideTest extends TestBase {
+
+    @Test
+    public void repositoryPageContainIssueButton() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+        open("https://github.com/");
+        $(".header-search-button").click();
+        $("#query-builder-test").setValue("eroshenkoam/allure-example").submit();
+        $(linkText("eroshenkoam/allure-example")).click();
+        $("#issues-tab").click();
+        $(withText("#87")).should(Condition.exist);
+    }
+}
---
+++ src/test/java/tests/StepsTest.java
@@ -0,0 +1,43 @@
+package tests;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+import static org.openqa.selenium.By.linkText;
+
+public class StepsTest extends TestBase {
+
+    private static final String repository = "eroshenkoam/allure-example";
+    private static final int issueNumber = 87;
+
+    @Test
+    public void repositoryPageContainIssueButtonWithLambaSteps() {
+        SelenideLogger.addListener("allure", new AllureSelenide());
+
+        step("Открывает главную страницу Github", () -> {
+            open("https://github.com/");
+        });
+        step("Клик на строку поиска", () -> {
+            $(".header-search-button").click();
+        });
+        step("Ввод и отправление введеного значения " + repository, () -> {
+            $("#query-builder-test").setValue(repository).submit();
+        });
+        step("Клик на репозиторий " + repository, () -> {
+            $(linkText("eroshenkoam/allure-example")).click();
+        });
+        step("Нажатие на Issue", () -> {
+            $("#issues-tab").click();
+        });
+        step("Проверка налачия Issue c #" + issueNumber, () -> {
+            $(withText("#" + issueNumber)).should(Condition.exist);
+        });
+
+    }
+}
